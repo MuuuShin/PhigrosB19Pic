21802e19c711259784a2669576fe5e42faad02db
暮心 <1248440677@qq.com>
2022-05-08 16:51:19 +0800

    demo1.5.8

--- data1.py
+++ data1.py
@@ -2,5 +2,6 @@ import sqlite3
 import pyDes
 
 # Complete the following code
+# The database need have 'songs2nex' table including 'id', 'sname', 'difficultyEZ', 'difficultyHD', 'difficultyIN', 'difficultyAT' columns.
 DES_SECRET_KEY = ''
 conn = sqlite3.connect('')
